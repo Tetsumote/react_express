eb3bf8eb1356cb48615285650e2d560659315f4c
Paweł Grajewski <unionproject88@gmail.com>
2019-10-29 17:24:13 +0100

    add redux branch

--- App.js
+++ App.js
@@ -1,48 +1,40 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { createStore } from 'redux'
 
-import List from './List'
-import Input from './Input'
-import Title from './Title'
-
-export default class App extends Component {
-
-  state = {
-    todos:[
-      'click to remove','Learn React Native','Write Code','Ship App'
-    ]
-  }
-  onAddTodo = (text) => {
-    const {todos} = this.state
-    this.setState({
-      todos:[text, ...todos]
-    })
-  }
-  onRemoveTodo = (index) => {
-    const {todos} = this.state
-    this.setState({
-      todos: todos.filter((todo, i) => i !== index),
-    })
-  }
-
-  render(){
-    const { todos } = this.state
-    return (
-      <View>
-        <Title>
-          To-do List
-        </Title>
-        <Input
-        placeholder={'Type a todo, then hit enter!'}
-        onSubmitEditing={this.onAddTodo}
-        />
-        <List
-        list={todos}
-        onPressItem={this.onRemoveTodo}
-        />
-      </View>
-    );
-  }
+// Define action types
+const types = {
+  INCREMENT: 'INCREMENT',
 }
 
+// Define a reducer
+const reducer = (state, action) => {
+  if (action.type === types.INCREMENT) {
+    return {count: state.count + 1}
+  }
+  return state
+}
 
+// Define the initial state of our store
+const initialState = {count: 0}
+
+// Create a store, passing our reducer function and our initial state
+const store = createStore(reducer, initialState)
+
+
+/// We're done! Redux is all set up. Here's how we can use it:
+
+
+// Now we can dispatch actions, which are understood by our store/reducer
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+
+// Calling `store.getState()` returns our state object
+export default function App() {
+  return (
+    <Text style={{fontSize: 100}}>
+      {store.getState().count}
+    </Text>
+  )
+}

d004b75bf2ae5dc98fc7904e74c7beda331d7054
Paweł Grajewski <unionproject88@gmail.com>
2019-10-31 18:12:52 +0100

    react redux

--- App.js
+++ App.js
@@ -1,54 +1,16 @@
 import React from 'react'
 import { View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import { actionCreators } from './todoListRedux'
-import List from './List'
-import Input from './Input'
-import Title from './Title'
+import App from './index.js'
 
 import store from './store'
 
-export default class App extends React.Component {
-
-  state = {}
-  unsubscribe = store.subscribe(() => {
-    const {todos} = store.getState()
-    this.setState({todos})
-  }) 
-  componentWillMount() {
-    const todos = store.getState().todos
-    this.setState({todos})
-  }
-
-  componentWillUnmount() {
-   unsubscribe()
-  }
-
-  onAddTodo = (text) => {
-    store.dispatch(actionCreators.add(text))
-  }
-
-  onRemoveTodo = (index) => {
-    store.dispatch(actionCreators.remove(index))
-  }
-
-  render() {
-    const {todos} = this.state
-
-    return (
-      <View>
-        <Title>
-          To-Do List
-        </Title>
-        <Input
-          placeholder={'Type a todo, then hit enter!'}
-          onSubmitEditing={this.onAddTodo}
-        />
-        <List
-          list={todos}
-          onPressItem={this.onRemoveTodo}
-        />
-      </View>
+export default function AppWithStore(){
+    return(
+        <Provider store={store}>
+            <App />
+        </Provider>
     )
-  }
 }
